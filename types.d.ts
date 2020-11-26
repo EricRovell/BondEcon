9a49536b4cd3ca3ef0f764e9e9dd0bd4ab67ddea
Eric Rovell <ericrovell@gmail.com>
2020-11-26 14:40:16 +0300

    added: express extended interface; Intl

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+declare namespace Express {
+  export interface Request {
+    session: any;
+  }
+  export interface Response {
+    session: any;
+  }
+}
+
+declare namespace Intl {
+  function getCanonicalLocales(locales: string | string[]): string[];
+
+  /* class RelativeTimeFormat {
+    constructor(locale: string);
+
+      // TODO Add other properties/methods
+  } */
+  const RelativeTimeFormat: any; // Use this instead of the class if you don't want to declare all properties/methods
+}
