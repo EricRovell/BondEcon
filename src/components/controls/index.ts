852cae41f9200aac1ae094416a7fd3da27fee799
Eric Rovell <ericrovell@gmail.com>
2021-01-02 15:48:55 +0300

    add: controls components index

---
+++ src/components/controls/index.ts
@@ -0,0 +1,4 @@
+export { default as Select } from "./Select.svelte";
+export { default as DatePicker } from "./DatePicker.svelte";
+export { default as InputText } from "./InputText.svelte";
+export { default as Button } from "./Button.svelte";
