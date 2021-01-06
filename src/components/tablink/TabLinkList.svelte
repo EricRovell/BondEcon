10b70d99d5821f686e7d7acba5af8636e334cd79
Eric Rovell <ericrovell@gmail.com>
2021-01-06 15:36:02 +0300

    upd: the final version

--- src/components/tablink/TabLinkList.svelte
+++ src/components/tablink/TabLinkList.svelte
@@ -1,63 +1,97 @@
 <script lang="ts" context="module">
-  function currentHash(node: HTMLElement, hash?: string) {
-    if (!hash) return;
-
-    (node.dataset.hash === hash)
-      ? node.setAttribute("aria-current", "page")
-      : node.removeAttribute("aria-current");
-
-    return {
-      update(hash?: string) {
-        (hash && node.dataset.hash === hash)
-          ? node.setAttribute("aria-current", "page")
-          : node.removeAttribute("aria-current");
-      }
-    };
+  interface Tab {
+    href: string;
+    label: string;
+    hash?: string;
   }
 </script>
 
 <script lang="ts">
   import { stores } from "@sapper/app";
+  import TabLink from "./TabLink.svelte";
+  
+  export let tabs: Tab[] = [];
+  export let fullWidth = false;
+  export let prefetch = false;
+  export let underline: "top" | "bottom" = "bottom";
+  export let exact = false;
 
   const { page } = stores();
 </script>
 
 <!--
   @component
-  Tabs
+  TabsLinkList Component
+  Wrapper for Links used as Tabs for navigation.
   
   Usage:
-    ```
-    
-    ```
+    - Tabs can be generated from object:
+      ```
+        <Tabs {tabs} />
+      ```
+    - Tabs can be included as slotted content. Component can provide value of the current URL to the slot:
+      ```
+        <Tabs let:url>
+          <Tab {url} />
+          <Tab {url} />
+        </Tabs>
+      ```
     
   Props:
   
-    | Name | type | default | description |
-    |:-----|:-----|:--------|:------------|
-    
+    | Name      | type              | default  | description                                                                  |
+    |:----------|:------------------|:---------|:-----------------------------------------------------------------------------|
+    | tabs      | Tab[]?            | []       | Tabs object. Used to construct tabs. If not provided, the slot is provided.  |
+    | fullWidth | boolean?          | false    | TabsList will take all the available width.                                  |
+    | prefetch  | boolean?          | false    | Controlling prefetching behaviour of tab links.                              |
+    | underline | "top" or "bottom" | "bottom" | How active links should be underlined with active state                      |
+    | exact     | boolean?          | false    | Active tabs href should be match with the url exactly.                       |
 -->
-<ul role="tablist">
-  <slot url={$page.path} />
+<ul
+  role="tablist"
+  class:full-width={fullWidth}
+  class:remove-border={underline === "top"}>
+    {#if tabs.length}
+      {#each tabs as { label, href }}
+        <TabLink
+          {href}
+          {underline}
+          {prefetch}
+          url={exact && $page.path}
+          active={!exact && `/${$page.path.split("/")[1]}` === href}
+        >
+          {label}
+        </TabLink>
+      {/each}
+    {:else}
+      <slot url={$page.path} />
+    {/if}
 </ul>
 
 <style>
   ul {
-    --height: 45px;
-
+    --height: 50px;
+    
     display: flex;
     justify-content: flex-start;
     align-items: center;
-
-    background-color: var(--color-1-500);
-    width: 100%;
+    
+    width: fit-content;
     height: var(--height);
+    background-color: var(--color-1-500);
     border-bottom: 1.5px solid hsl(var(--color-gray-800-str) / 0.15);
-    margin-bottom: 2.5em;
 
     position: sticky;
     top: 0;
 
     z-index: 5;
+  }
+  
+  .remove-border {
+    border-bottom: none;
+  }
+  
+  .full-width {
+    width: 100%;
   }
 </style>
