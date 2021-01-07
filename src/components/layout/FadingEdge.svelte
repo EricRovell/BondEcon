92668cbcb41b5c189fad1fdba5646a66940a68c6
Eric Rovell <ericrovell@gmail.com>
2021-01-07 16:46:18 +0300

    upd: moved to layout components

---
+++ src/components/layout/FadingEdge.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let top: string = "0px";
+  export let height: string = "1em";
+</script>
+
+<!--
+  @component
+  
+  Decorative sticky bar for creating fading edge effect.
+  
+  Usage:
+    <BlurryEdge top="50px" height="1em" />
+    
+  Props:
+    | Prop name | Type   | Default | Description           |
+    |-----------|--------|---------|-----------------------|
+    | top       | string | 0       | top stickiness offset |
+    | height    | string | 1em     | bar's height          |
+-->
+<div
+  class="gradient"
+  style="--top: {top}; --height: {height};"
+/>
+
+<style>
+  .gradient {
+    position: sticky;
+    top: var(--top);
+    
+    width: 100%;
+    height: var(--height);
+    
+    background: linear-gradient(180deg, var(--color-1-500), transparent);
+  }
+</style>
