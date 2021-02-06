7560422f3ba9f7500928aa9fa368899b9dfbb68b
Eric Rovell <ericrovell@gmail.com>
2021-02-06 03:34:43 +0300

    add: query builder index file

---
+++ src/services/query-builder/index.ts
@@ -0,0 +1,6 @@
+export { QueryBuilder } from "./queryBuilder";
+
+export { EcontwittQueryBuilder } from "./custom/econtwitt";
+export { ArticleQueryBuilder } from "./custom/article";
+
+export type { QueryParams, Options } from "./types";
