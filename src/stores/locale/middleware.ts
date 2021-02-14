38b4f9a6fbf4fdb0b9826ce1eef9a2399872d912
Eric Rovell <ericrovell@gmail.com>
2021-02-14 14:43:23 +0300

    add: locale store, server middleware

---
+++ src/stores/locale/middleware.ts
@@ -0,0 +1,72 @@
+import type { Request as ExpressRequest, Response as ExpressResponse, NextFunction } from "express";
+
+import { config } from "./config";
+import { locale } from "./locale";
+import { getCookie, setCookie } from "@util";
+import { getLocaleFromNavigator } from "./locale";
+
+
+// current locale
+let currentLocale: string;
+
+// update cookie on change
+locale.subscribe(value => {
+  if (!value) return;
+
+  currentLocale = value.slice(0, 2);
+
+  // client-side -> save the preference in a cookie
+  if (typeof window !== "undefined") {
+    const now = new Date();
+    now.setTime(now.getTime() + 7 * 24 * 3600 * 1000);
+
+    setCookie("locale", value.slice(0, 2), {
+      expires: now
+    });
+  }
+});
+
+/**
+ * Initialize the localization on client
+ */
+export function startClient() {
+  const localeValue = getCookie("locale") || getLocaleFromNavigator() || config.fallback;
+  locale.set(localeValue);
+}
+
+const DOCUMENT_REGEX = /^([^.?#@]+)?([?#](.+)?)?$/;
+
+export function localeMiddleware() {
+  startClient();
+
+  return (request: ExpressRequest, response: ExpressResponse, next: NextFunction) => {
+    const isDocument = DOCUMENT_REGEX.test(request.originalUrl);
+
+		// get the initial locale only for a document request
+		if (!isDocument) {
+      next();
+      return;
+    };
+    
+    let localeValue = getCookie("locale", request.headers.cookie);
+
+    if (!locale) {
+      if (request.headers["accept-language"]) {
+        const headerLang = request.headers["accept-language"].split(",")[0].trim();
+        if (headerLang.length > 1) {
+          localeValue = headerLang;
+        }
+      } else {
+        localeValue = config.initial || config.fallback;
+      }
+    }
+
+    if (localeValue && localeValue !== currentLocale) {
+      locale.set(localeValue);
+    }
+
+    request.session = { locale: currentLocale };
+
+    next();
+  }
+}
