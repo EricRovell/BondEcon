aec3f3809df5a952ba00cf6ac00dc6f1804084ef
Eric Rovell <ericrovell@gmail.com>
2021-02-14 14:31:37 +0300

    add: components/cards index

---
+++ src/components/cards/index.ts
@@ -0,0 +1,4 @@
+export { default as ProfileCard } from "./Profile.svelte";
+export { default as ExperienceCard } from "./Experience.svelte";
+export { default as EducationCard } from "./Education.svelte";
+export { default as ProjectCard } from "./Project.svelte";
