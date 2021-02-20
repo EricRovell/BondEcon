1479dc1400768ed5d7fd663ddaa6e5e232ff7a14
Eric Rovell <ericrovell@gmail.com>
2021-02-21 00:54:21 +0300

    add: Econtwitt content types

---
+++ src/components/content/econtwitt/types.ts
@@ -0,0 +1,12 @@
+export interface EcontwittRecord {
+  _id: string;
+  lang: "en" | "ru" | "fr";
+  date: string;
+  summary: string;
+  html: string;
+  md: string;
+  tags: string[];
+}
+
+export type EcontwittRecordCard = Omit<EcontwittRecord, "md">;
+export type EcontwittRecordPreview = Pick<EcontwittRecord, "_id" | "date" | "summary">;
