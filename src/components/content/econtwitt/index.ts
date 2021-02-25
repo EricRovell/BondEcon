c1557741f4b63ee90863302f5377e7109e5826b9
Eric Rovell <ericrovell@gmail.com>
2021-02-25 14:20:44 +0300

    add: Econtwitt index

---
+++ src/components/content/econtwitt/index.ts
@@ -0,0 +1,10 @@
+export { default as EcontwittCard } from "./EcontwittView.svelte";
+export { default as EcontwittPreview } from "./EcontwittPreview.svelte";
+
+export type {
+  EcontwittRecord,
+  EcontwittRecordCard,
+  EcontwittRecordPreview
+} from "./types";
+
+export { copy, share } from "./utils";
