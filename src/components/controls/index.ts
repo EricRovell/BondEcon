f3d16fed743e3e9afafb11d8b81576cf1468e4b5
Eric Rovell <ericrovell@gmail.com>
2021-02-28 02:03:45 +0300

    upd: updated exports

--- src/components/controls/index.ts
+++ src/components/controls/index.ts
@@ -4,3 +4,4 @@ export { default as InputText } from "./InputText.svelte";
 export { default as Button } from "./Button.svelte";
 export { default as RadioGroup } from "./Radio/RadioGroup.svelte";
 export { default as Form } from "./Form.svelte";
+export { default as SearchField } from "./SearchField.svelte";
