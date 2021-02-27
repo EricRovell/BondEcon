21d5b71c13c9f3d0f96f01b4a5e593c02cabb5ba
Eric Rovell <ericrovell@gmail.com>
2021-02-27 03:01:38 +0300

    upd: updated exports

--- src/components/controls/index.ts
+++ src/components/controls/index.ts
@@ -2,3 +2,5 @@ export { default as Select } from "./Select.svelte";
 export { default as DatePicker } from "./DatePicker.svelte";
 export { default as InputText } from "./InputText.svelte";
 export { default as Button } from "./Button.svelte";
+export { default as RadioGroup } from "./Radio/RadioGroup.svelte";
+export { default as Form } from "./Form.svelte";
