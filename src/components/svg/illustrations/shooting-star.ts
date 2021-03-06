0ab8e5eb1bb90fea750ea259b37489f357d6f30d
Eric Rovell <ericrovell@gmail.com>
2021-03-07 02:56:21 +0300

    add: shooting star illustration

---
+++ src/components/svg/illustrations/shooting-star.ts
@@ -0,0 +1,30 @@
+export const illustrationShootingStar = {
+  viewBox: "0 0 100 100",
+  name: "Shooting Star",
+  svg: `
+    <defs>
+      <linearGradient id="shooting-star-linear-gradient-1" x1="1257.48" y1="-505.08" x2="1260.1" y2="-505.08" gradientTransform="matrix(3.91, 0, 0, -3.91, -4851.8, -1933.94)" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#ac455a" stop-opacity="0.85"/>
+        <stop offset="0.92" stop-color="#ffa722" stop-opacity="0.77"/>
+      </linearGradient>
+      <linearGradient id="shooting-star-linear-gradient-2" x1="1257.08" y1="-497.52" x2="1259.69" y2="-497.52" xlink:href="#shooting-star-linear-gradient-1"/>
+      <linearGradient id="shooting-star-linear-gradient-3" x1="1263.68" y1="-503.71" x2="1266.3" y2="-503.71" xlink:href="#shooting-star-linear-gradient-1"/>
+      <linearGradient id="shooting-star-linear-gradient-4" x1="1244.69" y1="-506.69" x2="1264.24" y2="-506.69" gradientTransform="matrix(3.91, 0, 0, -3.91, -4852.04, -1933.47)" gradientUnits="userSpaceOnUse">
+        <stop offset="0" stop-color="#ac455a" stop-opacity="0.95"/>
+        <stop offset="0.95" stop-color="#ffa722" stop-opacity="0.77"/>
+      </linearGradient>
+      <linearGradient id="shooting-star-linear-gradient-5" x1="1151.64" y1="-418.27" x2="1155.43" y2="-418.27" gradientTransform="matrix(11.46, 3.99, 3.99, -11.46, -11521.83, -9322.68)" gradientUnits="userSpaceOnUse">
+        <stop offset="0.01" stop-color="#ac455a"/>
+        <stop offset="1" stop-color="#ffa722"/>
+      </linearGradient>
+    </defs>
+    <g style="stroke: #fff;stroke-linejoin: round;stroke-opacity: 0;stroke-width: 3px;fill-rule: evenodd;">
+      <path d="M64,40.52a5.11,5.11,0,1,1,5.11,5A5,5,0,0,1,64,40.52Z" style="fill: url(#shooting-star-linear-gradient-1)"/>
+      <path d="M62.41,11a5.11,5.11,0,0,1,10.22,0,5.11,5.11,0,0,1-10.22,0Z" style="fill: url(#shooting-star-linear-gradient-2)"/>
+      <path d="M88.22,35.16a5.12,5.12,0,1,1,5.12,5A5,5,0,0,1,88.22,35.16Z" style="fill: url(#shooting-star-linear-gradient-3)"/>
+      <path d="M52.83,13.17" style="fill: #43102c;stroke-width: 1.6px;"/>
+      <path d="M13.75,49.54l13.46,8.94,15.3-5.69L37.8,68,48.46,80.4,88.68,46.88A6.51,6.51,0,0,0,90,44.55a4.47,4.47,0,0,0-.16-2.74,4.89,4.89,0,0,0-1.33-2,6.34,6.34,0,0,0-2.44-1.3,6.1,6.1,0,0,0-3.82.38,10.08,10.08,0,0,0-2.45,1.82l-21,17.94-9.61,8.17-2.63,2.26c-.63.54-2.1-.18-1.14-1.18s2.34-2.28,3.9-3.73c3.64-3.39,10.51-8.94,10.51-8.94a37.8,37.8,0,0,0,3.87-3.13A5.37,5.37,0,0,0,64.93,47,6,6,0,0,0,61.8,44a5.08,5.08,0,0,0-5.14.46c-2.16,1.67-3.64,3.05-6.91,5.82-2,1.67-2.28,2-2.7,2.2s-1.07.18-1.28-.55,1-1.48,1-1.48l6.95-5.79,7.12-6.08s2.06-2.06,1.85-4a5.29,5.29,0,0,0-2.59-4.2c-1.58-1.11-4.42-.58-5.1-.22a18.59,18.59,0,0,0-3.74,2.81l-9,7.63L36.53,45.3s-1.69,1.58-2.15,1.48a.61.61,0,0,1-.4-.92c1.06-1.15,7.62-6.74,7.62-6.74L59.34,24s2.63-1.78,3-3.87A5,5,0,0,0,60,14.92c-2-1.36-5.07-.56-6,.13C52.07,16.34,13.75,49.54,13.75,49.54Z" style="fill: url(#shooting-star-linear-gradient-4)"/>
+      <path d="M1.55,75.15l13-9.78-.76-15.83,13.53,8.91,15.24-5.67-4.38,15.1L48.47,80.4l-16.33.47L23.29,94l-5.9-14.92Z" style="fill: url(#shooting-star-linear-gradient-5)"/>
+    </g>
+  `
+};
