1179ad6dbc37c2238a149a92d25b4f24388a1ae8
Eric Rovell <ericrovell@gmail.com>
2021-03-26 20:17:29 +0300

    add: navigation mobile data

---
+++ src/bondecon/meta/navigation-bottom.ts
@@ -0,0 +1,27 @@
+import { iconQuill, iconMagicWand, iconHeels, iconMagic } from "@svg";
+import type { NavigationItem } from "../types";
+
+export const navigation: NavigationItem[] = [
+  {
+    label: "blog",
+    href: "/blog",
+    icon: iconQuill,
+    pattern: /blog\/*/
+  },
+  {
+    label: "cv",
+    href: "/cv",
+    icon: iconHeels,
+    pattern: /cv\/*/
+  },
+  {
+    label: "home",
+    href: "/",
+    icon: iconMagicWand,
+  }
+];
+
+export const menuButton = {
+  label: "menu",
+  icon: iconMagic
+};
