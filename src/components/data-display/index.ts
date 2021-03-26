af22170090cf1d985f12bee8cad85510f8cfffef
Eric Rovell <ericrovell@gmail.com>
2021-03-26 19:43:51 +0300

    add: data-display components group index

---
+++ src/components/data-display/index.ts
@@ -0,0 +1,5 @@
+export { default as Article } from "./Article.svelte";
+export { default as Chip } from "./Chip.svelte";
+export { default as DateTime } from "./DateTime.svelte";
+export { default as Heading } from "./Heading.svelte";
+export { Timeline, TimelineRecord} from "./timeline";
