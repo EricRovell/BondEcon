aa962e4fa0a7a5e4cb0b872c319c3972bbb3fe94
Eric Rovell <ericrovell@gmail.com>
2021-03-28 11:29:45 +0300

    add: article publications api endpoint

---
+++ src/routes/api/blog/articles/index.json.ts
@@ -0,0 +1,30 @@
+import { database } from "#db";
+import { ArticleQueryBuilder } from "@services/query-builder";
+import type { SapperRequest, SapperResponse } from "@sapper/server";
+import type { ArticleRecordCard } from "@components/content/article";
+
+/**
+ * Articles API endpoint.
+ */
+export async function get(request: SapperRequest, response: SapperResponse, next: () => void) {
+  const { db } = await database();
+  
+  const queryBuilder = new ArticleQueryBuilder({ ...request.query, projection: "card" });
+  const [ query, options ] = queryBuilder.buildQuery();
+  
+  try {
+    const data: ArticleRecordCard[] | null = await db?.collection("blog.articles")
+      .find(query, options)
+      .toArray() ?? null;
+
+    if (data) {
+      response.setHeader("Content-Type", "application/json");
+      response.end(JSON.stringify(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error);
+    next();
+  } 
+}
