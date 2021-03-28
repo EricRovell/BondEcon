3e7bfe76dacdadc1c45ff8d01541ef53c27fcafa
Eric Rovell <ericrovell@gmail.com>
2021-03-28 11:29:55 +0300

    add: econtwitt publications api endpoint

---
+++ src/routes/api/blog/econtwitts/index.json.ts
@@ -0,0 +1,30 @@
+import { database } from "#db";
+import { EcontwittQueryBuilder } from "@services/query-builder";
+import type { SapperRequest, SapperResponse } from "@sapper/server";
+import type { EcontwittRecord } from "@components/content/econtwitt";
+
+/**
+ * Econtwitts publications API endpoint.
+ */
+export async function get(request: SapperRequest, response: SapperResponse, next: () => void) {
+  const { db } = await database();
+  const queryBuilder = new EcontwittQueryBuilder(request.query);
+  
+  const [ query, options ] = queryBuilder.buildQuery();
+  
+  try {
+    const data: EcontwittRecord[] | null = await db?.collection("blog.econtwitts")
+      .find(query, options)
+      .toArray() ?? null;
+
+    if (data) {
+      response.setHeader("Content-Type", "application/json");
+      response.end(JSON.stringify(data));
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.error(error)
+    next();
+  } 
+}
