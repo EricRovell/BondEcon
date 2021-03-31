b236ddfe3a97066e0f33eb77239b1c3385950388
Eric Rovell <ericrovell@gmail.com>
2021-04-01 00:11:19 +0300

    add: helpers index

---
+++ src/util/helpers/index.ts
@@ -0,0 +1,7 @@
+export { abbreviateNumber } from "./abbreviate-number";
+export { controlPageScroll } from "./control-page-scroll";
+export { copyToClipboard } from "./clipboard";
+export { fetchAll } from "./fetchAll";
+export { flatObject } from "./flat-object";
+export { getCookie, setCookie } from "./manage-cookies";
+export { webShare } from "./share";
