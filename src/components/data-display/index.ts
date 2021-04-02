3c7df906d35d8e69925db92c27dbf96e72803464
Eric Rovell <ericrovell@gmail.com>
2021-04-02 22:35:28 +0300

    upd: updated export paths

--- src/components/data-display/index.ts
+++ src/components/data-display/index.ts
@@ -3,3 +3,4 @@ export { default as Chip } from "./Chip.svelte";
 export { default as DateTime } from "./DateTime.svelte";
 export { default as Heading } from "./Heading.svelte";
 export { Timeline, TimelineRecord} from "./timeline";
+export { default as SearchResult } from "./SearchResult.svelte";
