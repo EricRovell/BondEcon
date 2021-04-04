ca4ff9e91acab752fb6dedd5ab02367c1e965ab5
Eric Rovell <ericrovell@gmail.com>
2021-04-04 12:18:32 +0300

    upd: importing global styles for bundling

--- src/client.ts
+++ src/client.ts
@@ -1,6 +1,12 @@
 import * as sapper from "@sapper/app";
 import { startClient } from "@stores/locale";
 
+import "@styles/normalize.css";
+import "@styles/main.css";
+import "@styles/typography.css";
+import "@styles/theme-light.css";
+import "@styles/theme-dark.css";
+
 startClient();
 
 sapper.start({
