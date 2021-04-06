f2cdbea9b520bc9c4bc02702fe77aeaee4ad0c4b
Eric Rovell <ericrovell@gmail.com>
2021-04-06 12:32:53 +0300

    add: locale redirection route

---
+++ src/routes/index.svelte
@@ -0,0 +1,12 @@
+<script context="module" lang="ts">
+  import type { Preload } from "@sapper/common";
+  import { supportsLocale } from "@stores/locale";
+  
+  export const preload: Preload = async function(this, page, session) {
+    const locale = supportsLocale(session?.locale)
+      ? session.locale
+      : "en";    
+
+    return this.redirect(301, `/${locale}/`);
+  }
+</script>
