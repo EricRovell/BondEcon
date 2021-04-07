aeed1d04e235f89f0133cf4661a8573782c23c6a
Eric Rovell <ericrovell@gmail.com>
2021-04-07 12:53:35 +0300

    upd: pages paths

--- src/core/routes.ts
+++ src/core/routes.ts
@@ -3,6 +3,13 @@ import { locale } from "@stores";
 
 export const basePath = derived(locale, $locale => `/${$locale}`);
 
+// pages
+export const blogPagePath = derived(locale, $locale => `${$locale}/blog`);
+export const articlesPagePath = derived(blogPagePath, $path => `${$path}/articles`);
+export const econtwittsPagePath = derived(blogPagePath, $path => `${$path}/econtwitts`);
+export const cvPagePath = derived(locale, $locale => `${$locale}/cv`);
+export const aboutPagePath = derived(locale, $locale => `${$locale}/about`);
+
 // contents
 export const articlePath = derived(basePath, $path => (id: string) => `${$path}/blogpost/article-${id}`);
 export const econtwittPath = derived(basePath, $path => (id: string) => `${$path}/blogpost/econtwitt-${id}`);
