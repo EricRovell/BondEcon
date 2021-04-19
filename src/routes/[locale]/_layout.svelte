3b74911cad5027c17bad37f3582be4a268409b6a
Eric Rovell <ericrovell@gmail.com>
2021-04-19 11:58:37 +0300

    add: layout for registering locale dicts

---
+++ src/routes/[locale]/_layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts" context="module">
+  import type { Preload } from "@sapper/common";
+  import { registerDictionaries } from "@stores/locale";
+
+  export const preload: Preload = async function(this, page, session) {  
+    await registerDictionaries([
+      { locale: "en", url: "locale/en/ui.json" },
+      { locale: "ru", url: "locale/ru/ui.json" }
+    ], page.params.locale ?? session?.locale);
+  }
+</script>
+
+<slot />
