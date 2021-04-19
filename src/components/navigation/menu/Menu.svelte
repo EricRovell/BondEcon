ef93a2c7276af05fc25b8ffe12ce4981cbd95a5c
Eric Rovell <ericrovell@gmail.com>
2021-04-19 12:30:42 +0300

    upd: simplified structure

--- src/components/navigation/menu/Menu.svelte
+++ src/components/navigation/menu/Menu.svelte
@@ -3,7 +3,8 @@
   import { setContext } from "svelte";
   
   export let wide = false;
-  export let noBorder = false;
+  export let border = false;
+  export let spacing: undefined | "equal" = undefined;
   
   const { page } = stores();
   
@@ -33,39 +34,42 @@
   Context:
     - "page": sapper page store;
 -->
-<nav
-  class:wide
-  class:noBorder>
-    <ul>
-      <slot />
-    </ul>
+<nav class:border class:wide>
+  <ul class:equal-spacing={spacing === "equal"}>
+    <slot />
+  </ul>
 </nav>
 
 <style>
   nav {
     --navigation-height: 50px;
-    
-    background-color: var(--navigation-background, transparent);
-    border-bottom: 1.5px solid hsl(var(--gray-h) var(--gray-s-800) var(--gray-l-800) / 0.15);
-    position: sticky;
-    top: 0;
-    z-index: 5;
   }
   
   ul {
-    display: flex;
-    justify-content: flex-start;
-    align-items: center;
+    display: inline-grid;
+    place-items: center;
+    place-content: center start;
+    grid-auto-flow: column;
+    gap: var(--spacing-3, 0.75em);
     
     width: fit-content;
-    height: var(--navigation-height);   
+    height: var(--navigation-height);
   }
   
-  .noBorder {
-    border-bottom: none;
+  .border {
+    border-bottom: 1.5px solid hsl(var(--gray-h) var(--gray-s-800) var(--gray-l-800) / 0.15);
   }
   
   .wide {
     width: 100%;
+  }
+  
+  .wide > ul {
+    display: grid;
+    width: 100%;
+  }
+  
+  .equal-spacing {
+    grid-auto-columns: 1fr;
   }
 </style>
