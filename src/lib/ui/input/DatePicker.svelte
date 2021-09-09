53ffa9f45804948f731181791dd38d573a0635f6
Eric Rovell <ericrovell@gmail.com>
2021-09-09 14:45:36 +0300

    improved date type

--- src/lib/ui/input/DatePicker.svelte
+++ src/lib/ui/input/DatePicker.svelte
@@ -1,6 +1,16 @@
+<script context="module" lang="ts">
+  export type DateInputType =
+    "datetime-local" |
+    "month" |
+    "time" |
+    "week" |
+    "date";
+</script>
+
 <script lang="ts">
   export let label: string;
   export let name: string | undefined = undefined;
+  export let type: DateInputType = "date";
   export let value: string | undefined = undefined;
   export let min: string | undefined = undefined;
   export let max: string = new Date().toISOString().substring(0, 10);
@@ -29,14 +39,20 @@
   | min       | string | undefined    | self-explanatory |
   | max       | string | current date | self-explanatory |
 -->
+<!-- svelte-ignore a11y-label-has-associated-control -->
 <label>
   <span>{label}:</span>
-  <input
-    type="date"
-    bind:value
-    {name}    
-    {min}
-    {max} />
+  {#if type === "datetime-local"}
+    <input type="datetime-local" bind:value {name} {min} {max} />
+  {:else if type === "month"}
+    <input type="month" bind:value {name} {min} {max} />
+  {:else if type === "week"}
+    <input type="week" bind:value {name} {min} {max} />
+  {:else if type === "time"}
+    <input type="week" bind:value {name} {min} {max} />
+  {:else}
+    <input type="date" bind:value {name} {min} {max} />
+  {/if}
 </label>
 
 <style>
@@ -52,22 +68,22 @@
     margin-bottom: var(--spacing-2, 0.5em);
   }
 
-  input[type="date"] {
+  input {
     --shadow-size: 0;
     
     background: var(--color-surface-400);
     color: var(--color-gray-700);
     border: 1px solid var(--color-gray-500);
     border-radius: var(--radius-small);
-    padding: var(--spacing-1);
+    padding: var(--spacing-2, 0.5em);
     width: 100%;
     
     box-shadow: 0 0 0 var(--shadow-size) var(--color-primary);
     transition: box-shadow 0.15s linear;
   }
 
-  input[type="date"]:focus,
-  input[type="date"]:hover {
+  input:focus,
+  input:hover {
     --shadow-size: 1px;  
   }
 
