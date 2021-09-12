c6e3112179c51c1a0b4ed3ba6cdcbddb7a2d646d
Eric Rovell <ericrovell@gmail.com>
2021-09-13 00:07:14 +0300

    define about page

--- src/routes/[locale]/about.svelte
+++ src/routes/[locale]/about.svelte
@@ -1,3 +1,17 @@
-<h1>
-  About page
-</h1>
+<script context="module">
+	export async function load({ page }) {
+		const Post = (await import(`../content/about-${page.params.locale}.md`)).default;
+
+		return {
+			props: {
+				Post
+			}
+		};
+	}
+</script>
+
+<script>
+	export let Post;
+</script>
+
+<Post />
