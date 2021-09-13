fdfdcbe56580ac9bf7034aa223e9889e486a4f27
Eric Rovell <ericrovell@gmail.com>
2021-09-13 12:33:01 +0300

    update blog content handling

---
+++ src/routes/[locale]/blog/[slug].svelte
@@ -0,0 +1,17 @@
+<script context="module">
+	export async function load({ page }) {
+		const Post = (await import(`../../content/${page.params.slug}.md`)).default;
+
+		return {
+			props: {
+				Post
+			}
+		};
+	}
+</script>
+
+<script>
+	export let Post;
+</script>
+
+<Post />
