5ad5fc55990ca4f7269fa420d3192c5c655e8b17
Eric Rovell <ericrovell@gmail.com>
2021-09-13 12:29:42 +0300

    update home page

--- src/routes/[locale]/home.svelte
+++ src/routes/[locale]/home.svelte
@@ -1,3 +1,26 @@
-<h1>
-  Home page
-</h1>
+<script>
+  import { page } from "$app/stores";
+  import { _, locale } from "svelte-i18n";
+  import { SVG, girlWithDog } from "@components/svg";
+
+  $: $locale = $page.params.locale;
+</script>
+
+<main>
+  <h1>{$_("message.welcome")}</h1>
+  <SVG data={girlWithDog} size="400px" />
+  <p>{$_("message.wip:page")}</p>
+</main>
+
+<style>
+  main {
+    display: grid;
+    justify-items: center;
+    width: 100%;
+    margin-top: 10vh;
+  }
+
+  p {
+    font-size: var(--font-size-7);
+  }
+</style>
